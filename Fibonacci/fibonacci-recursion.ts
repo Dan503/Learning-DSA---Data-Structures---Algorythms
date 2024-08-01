04363d7143988379020cd87749b39781cc556b5e
Daniel Tonon <dan503.npm@gmail.com>
2024-08-01 12:18:15 +1000

    Made successful recursive fibonacci array function

--- Fibonacci/fibonacci-recursion.ts
+++ Fibonacci/fibonacci-recursion.ts
@@ -1,19 +1,17 @@
-// export function fibonacciRecursion(
-// 	numberOfItems: number,
-// 	currentIndex: number,
-// 	currentValue: Array<number> = [0, 1],
-// 	previous2Values: [number, number],
-// ): Array<number> {
-// 	const nextNumber = getFibonacciNumber()
+export function fibonacciRecursion(
+	numberOfItems: number,
+	// currentIndex: number,
+	currentValue: Array<number> = [0, 1],
+	previous2Values: [number, number] = [0, 1],
+): Array<number> {
+	const newValue = previous2Values[0] + previous2Values[1]
+	currentValue.push(newValue)
+	if (numberOfItems > currentValue.length) {
+		fibonacciRecursion(numberOfItems, currentValue, [
+			previous2Values[1],
+			newValue,
+		])
+	}
 
-// 	if (currentValue.length < numberOfItems) {
-// 		const index = currentValue.length - 1
-// 		result = currentValue.push(currentValue)
-// 	}
-
-// 	return result
-// }
-
-// function getFibonacciNumber(previous2Numbers: [number, number]) {
-// 	return previous2Numbers[0] + previous2Numbers[1]
-// }
+	return currentValue
+}
--- Fibonacci/fibonacci.test.ts
+++ Fibonacci/fibonacci.test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from 'jsr:@std/assert@1/equals'
 import { fibonacciLoop } from './fibonacci-loop.ts'
 // import { fibonacciFind } from './fibonacci-find.ts'
-// import { fibonacciRecursion } from './fibonacci-recursion.ts'
+import { fibonacciRecursion } from './fibonacci-recursion.ts'
 
 const fibonacciSequence = [
 	0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597,
@@ -16,13 +16,13 @@ Deno.test('fibonacci loop', () => {
 	)
 })
 
-// Deno.test('fibonacci recursion', () => {
-// 	assertEquals(
-// 		fibonacciRecursion(fibonacciSequence.length),
-// 		fibonacciSequence,
-// 		`recurse`,
-// 	)
-// })
+Deno.test('fibonacci recursion', () => {
+	assertEquals(
+		fibonacciRecursion(fibonacciSequence.length),
+		fibonacciSequence,
+		`recurse`,
+	)
+})
 
 // Deno.test('fibonacci find', () => {
 // 	fibonacciSequence.forEach((number, index) => {
