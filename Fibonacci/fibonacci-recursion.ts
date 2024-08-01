abc769b749e4233854fcc36cf1a561e2790bb348
Daniel Tonon <dan503.npm@gmail.com>
2024-08-01 12:42:28 +1000

    cleaner fibonacciRecursion code

--- Fibonacci/fibonacci-recursion.ts
+++ Fibonacci/fibonacci-recursion.ts
@@ -1,17 +1,16 @@
+const sequence = [0, 1]
+
 export function fibonacciRecursion(
 	numberOfItems: number,
-	// currentIndex: number,
-	currentValue: Array<number> = [0, 1],
-	previous2Values: [number, number] = [0, 1],
+	prev1: number = sequence[0],
+	prev2: number = sequence[1],
 ): Array<number> {
-	const newValue = previous2Values[0] + previous2Values[1]
-	currentValue.push(newValue)
-	if (numberOfItems > currentValue.length) {
-		fibonacciRecursion(numberOfItems, currentValue, [
-			previous2Values[1],
-			newValue,
-		])
+	const newValue = prev1 + prev2
+	sequence.push(newValue)
+
+	if (numberOfItems > sequence.length) {
+		fibonacciRecursion(numberOfItems, prev2, newValue)
 	}
 
-	return currentValue
+	return sequence
 }
