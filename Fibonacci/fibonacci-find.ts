f9aceac7892eb5ed46524ef1e9e20c62edaf846a
Daniel Tonon <dan503.npm@gmail.com>
2024-08-01 13:25:41 +1000

    fibonacciFind updated to match tutorial

--- Fibonacci/fibonacci-find.ts
+++ Fibonacci/fibonacci-find.ts
@@ -1,6 +1,9 @@
-import { fibonacciRecursion } from './fibonacci-recursion.ts'
-// O(n)
+// O(2^n) // 2nd worst type of efficiency in algorithms!
+// The much better method is to generate the list of fibonacci numbers first then pick out the index
 export function fibonacciFind(index: number): number {
-	const sequence = fibonacciRecursion(index)
-	return sequence[index]
+	if (index <= 1) {
+		return index
+	}
+
+	return fibonacciFind(index - 1) + fibonacciFind(index - 2)
 }
