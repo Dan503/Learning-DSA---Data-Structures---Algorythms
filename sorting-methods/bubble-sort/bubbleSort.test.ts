fdcf88789b500209d55948350c1ba47e9978d719
Daniel Tonon <dan503.npm@gmail.com>
2024-08-05 23:29:37 +1000

    Renamed bubbleSort file

--- sorting-methods/bubble-sort/bubbleSort.test.ts
+++ sorting-methods/bubble-sort/bubbleSort.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from 'jsr:@std/assert@1'
 import { bubbleSortSinglePass } from './0-bubbleSort-singlePass.ts'
-import { bubbleSortContanerLoop } from './1-bubbleSort-containerLoop.ts'
+import { bubbleSort } from './1-bubbleSort.ts'
 
 Deno.test('bubble sort single pass', () => {
 	const input = [7, 12, 9, 11, 3]
@@ -17,9 +17,5 @@ Deno.test('bubble sort full version', () => {
 	const input = [7, 12, 9, 11, 3]
 	const expectedOutput = [3, 7, 9, 11, 12]
 
-	assertEquals(
-		bubbleSortContanerLoop(input),
-		expectedOutput,
-		'Bubble single pass',
-	)
+	assertEquals(bubbleSort(input), expectedOutput, 'Bubble single pass')
 })
