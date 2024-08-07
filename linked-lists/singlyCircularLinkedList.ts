c62d964463d376edb06c652f156afb2a3cab8d2d
Daniel Tonon <dan503.npm@gmail.com>
2024-08-07 12:53:13 +1000

    Created a `createSinglyLinkedList()` function
    This ends up using up even more memory because it is storing the linked list items inside an array. the array takes up memory and the linked list also takes up memory

--- linked-lists/singlyCircularLinkedList.ts
+++ linked-lists/singlyCircularLinkedList.ts
@@ -1,14 +1,20 @@
-import { SinglyNode } from './singlyLinkedList.ts'
+import { createSinglyLinkedList } from './singlyLinkedList.ts'
 
-const node1 = new SinglyNode(3)
-const node2 = new SinglyNode(5)
-const node3 = new SinglyNode(13)
-const node4 = new SinglyNode(2)
+const { headNode, tailNode } = createSinglyLinkedList([3, 5, 13, 2], {
+	isCircular: true,
+})
 
-node1.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node1
+// The long way of making a circular Singly linked list:
 
-export const singlyCircularNodeHead = node1
-export const singlyCircularNodeTail = node4
+// const node1 = new SinglyNode(3)
+// const node2 = new SinglyNode(5)
+// const node3 = new SinglyNode(13)
+// const node4 = new SinglyNode(2)
+
+// node1.next = node2
+// node2.next = node3
+// node3.next = node4
+// node4.next = node1
+
+export const singlyCircularNodeHead = headNode
+export const singlyCircularNodeTail = tailNode
--- linked-lists/singlyLinkedList.ts
+++ linked-lists/singlyLinkedList.ts
@@ -7,14 +7,43 @@ export class SinglyNode {
 	}
 }
 
-const node1 = new SinglyNode(3)
-const node2 = new SinglyNode(5)
-const node3 = new SinglyNode(13)
-const node4 = new SinglyNode(2)
+/**
+ * Shouldn't actually use arrays when creating linked lists.
+ * This is because it ends up using twice as much memory to store the linked list.
+ * Using this because I need to make a lot of linked lists, I don't want to do it manually, and I'm using small numbers of elements.
+ */
+export function createSinglyLinkedList(
+	numbers: Array<number>,
+	{ isCircular = false }: { isCircular?: boolean } = {},
+) {
+	const listItems = numbers.map((number) => new SinglyNode(number))
 
-node1.next = node2
-node2.next = node3
-node3.next = node4
+	listItems.forEach((item, index) => {
+		item.next = listItems[index + 1] || null
+	})
 
-export const singlyNodeHead = node1
-export const singlyNodeTail = node4
+	if (isCircular) {
+		listItems[listItems.length - 1].next = listItems[0]
+	}
+
+	return {
+		headNode: listItems[0],
+		tailNode: listItems[listItems.length - 1],
+	}
+}
+
+const { headNode, tailNode } = createSinglyLinkedList([3, 5, 13, 2])
+
+// The long way of creating a Singly linked list
+
+// const node1 = new SinglyNode(3)
+// const node2 = new SinglyNode(5)
+// const node3 = new SinglyNode(13)
+// const node4 = new SinglyNode(2)
+
+// node1.next = node2
+// node2.next = node3
+// node3.next = node4
+
+export const singlyNodeHead = headNode
+export const singlyNodeTail = tailNode
