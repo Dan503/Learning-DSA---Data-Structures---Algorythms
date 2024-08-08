84de0304e50e1604567009d6a7ab908beb2eb0be
Daniel Tonon <dan503.npm@gmail.com>
2024-08-08 14:24:23 +1000

    Handling the reverse case for swapNodes function

--- linked-lists/list-operations/swapNodes.ts
+++ linked-lists/list-operations/swapNodes.ts
@@ -18,6 +18,8 @@ export function swapNodes(node1: DoublyNode, node2: DoublyNode) {
 
 	// Apply the new prev and next values to the nodes
 	const areSwapNodesAdjacent = prevNodeB === node1
+	const areSwapNodesAdjacent_reverse = prevNodeA === node2
+
 	if (areSwapNodesAdjacent) {
 		// node1 and node2 are directly next to each other
 		node1.prev = node2
@@ -29,6 +31,15 @@ export function swapNodes(node1: DoublyNode, node2: DoublyNode) {
 		if (prevNodeA) prevNodeA.next = node2
 		if (nextNodeB) nextNodeB.prev = node1
 		// if (nextNodeB) nextNodeB.prev = node1
+	} else if (areSwapNodesAdjacent_reverse) {
+		node2.prev = node1
+		node1.next = node2
+
+		node2.next = nextNodeA
+		node1.prev = prevNodeB
+
+		if (prevNodeB) prevNodeB.next = node1
+		if (nextNodeA) nextNodeA.prev = node2
 	} else {
 		// node1 and node2 are unrelated to each other
 		node1.prev = prevNodeB
@@ -53,7 +64,7 @@ const { allNodes } = createDoublyLinkedList([1, 2, 3, 4, 5], {
 })
 const [node1, node2, node3] = allNodes
 
-swapNodes(node2, node3)
+swapNodes(node3, node2)
 
 const outputItems: Array<number> = []
 let i = 0
