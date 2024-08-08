489cc172266020ed6d281f6b7bc1608eb464b8ef
Daniel Tonon <dan503.npm@gmail.com>
2024-08-08 21:01:44 +1000

    Singly node sort is working... it is using selection sort though, not bubble sort

--- linked-lists/list-operations/linkedListBubbleSort.ts
+++ linked-lists/list-operations/linkedListBubbleSort.ts
@@ -2,42 +2,33 @@ import {
 	createSinglyLinkedList,
 	SinglyNode,
 } from '../list-types/singlyLinkedList.ts'
+import { logLinkedList } from './logLinkedList.ts'
 import { traverseForward } from './traversal.ts'
 
 export function linkedListBubbleSort(headNode: SinglyNode) {
-	let lastNode: SinglyNode | undefined = undefined
 	traverseForward(headNode, (node) => {
-		lastNode = bubbleSortSinglePass(node)
+		bubbleSortSinglePass(node)
 	})
 }
 
-function bubbleSortSinglePass(
-	startNode: SinglyNode,
-	// endNode: SinglyNode | undefined,
-) {
-	let finalNode: SinglyNode = startNode
+function bubbleSortSinglePass(startNode: SinglyNode) {
 	traverseForward(startNode, (thisNode) => {
-		const nextNode = thisNode.next
+		const startValue = startNode.data
 		const thisValue = thisNode.data
-		const nextValue = nextNode?.data || -Infinity
 		// if current value is greater than next value, swap them
-		if (thisValue > nextValue && nextNode) {
-			thisNode.data = nextValue
-			nextNode.data = thisValue
+		if (startValue > thisValue) {
+			// Note that we swap the data values instead of the node connections
+			// without prev connections, it is too difficult keeping track of correct linkages
+			startNode.data = thisValue
+			thisNode.data = startValue
 		}
-		finalNode = thisNode
 	})
-	return finalNode
 }
 
-const { headNode } = createSinglyLinkedList([1, 4, 3, 5, 2], {
+const { headNode } = createSinglyLinkedList([1, 4, 3, 6, 2, 5], {
 	isCircular: false,
 })
 
 linkedListBubbleSort(headNode)
 
-const outputItems: Array<number> = []
-traverseForward(headNode, (node) => {
-	outputItems.push(node.data)
-})
-console.log(outputItems)
+logLinkedList('Bubble sort', headNode)
